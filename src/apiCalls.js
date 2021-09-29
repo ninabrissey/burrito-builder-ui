98c02af1618950d44bf22ea14a6a8ca53fd7c724
Nina Brissey <nbrissey@gmail.com>
2021-09-29 08:29:14 -0700

    Change fetch call to be more semantic

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,4 +1,5 @@
-export const getOrders = () => {
-  return fetch('http://localhost:3001/api/v1/orders')
-      .then(response => response.json())
-}
+export const fetchOrders = () => {
+  return fetch('http://localhost:3001/api/v1/orders').then((response) =>
+    response.json()
+  );
+};
