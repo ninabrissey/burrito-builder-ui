108888df25f56941b3fd939d7a807c76c5ed6050
Nina Brissey <nbrissey@gmail.com>
2021-09-29 09:56:10 -0700

    Add addOrder fucntion

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { fetchOrders } from '../../apiCalls';
+import { fetchOrders, postOrder } from '../../apiCalls';
 import Orders from '../../components/Orders/Orders';
 import OrderForm from '../../components/OrderForm/OrderForm';
 import './App.css';
@@ -11,6 +11,16 @@ const App = () => {
     const res = await fetchOrders();
     const ordersData = await res.json();
     setOrders(ordersData.orders);
+  };
+
+  const addOrder = async (newOrder) => {
+    try {
+      const res = await postOrder(newOrder);
+      const postedOrder = await res.json();
+      setOrders([...orders, postedOrder]);
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   useEffect(() => {
@@ -25,7 +35,7 @@ const App = () => {
     <main className="App">
       <header>
         <h1>Burrito Builder</h1>
-        <OrderForm />
+        <OrderForm addOrder={addOrder} />
       </header>
       <Orders orders={orders} />
     </main>
