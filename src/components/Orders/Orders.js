b487fbb8b07b410e0cc935b28a6ae67c1d74131a
Nina Brissey <nbrissey@gmail.com>
2021-09-29 09:07:47 -0700

    Change ordersList to a function

--- src/components/Orders/Orders.js
+++ src/components/Orders/Orders.js
@@ -2,22 +2,23 @@ import React from 'react';
 import './Orders.css';
 
 const Orders = ({ orders }) => {
-  const orderList = orders.map((order) => {
-    return (
-      <div className="order">
-        <h3>{order.name}</h3>
-        <ul className="ingredient-list">
-          {order.ingredients.map((ingredient) => {
-            return <li>{ingredient}</li>;
-          })}
-        </ul>
-      </div>
-    );
-  });
+  console.log(orders);
+  const orderList = () => {
+    return orders.map((order) => {
+      return (
+        <div className="order">
+          <h3>{order.name}</h3>
+          <ul className="ingredient-list">
+            {order.ingredients.map((ingredient) => {
+              return <li>{ingredient}</li>;
+            })}
+          </ul>
+        </div>
+      );
+    });
+  };
 
-  return (
-    <section>{orderList.length ? orderList : <p>No orders yet!</p>}</section>
-  );
+  return <section>{orders ? orderList() : <p>You have no orders</p>}</section>;
 };
 
 export default Orders;
