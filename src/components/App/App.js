921ea9b9f51a33005939d3fbe137ad7d548010af
Nina Brissey <nbrissey@gmail.com>
2021-09-29 08:28:56 -0700

    Convert to using hooks

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,30 +1,34 @@
-import React, { Component } from 'react';
-import './App.css';
-import { getOrders } from '../../apiCalls';
+import { useState, useEffect } from 'react';
+import { fetchOrders } from '../../apiCalls';
 import Orders from '../../components/Orders/Orders';
 import OrderForm from '../../components/OrderForm/OrderForm';
+import './App.css';
 
-class App extends Component {
-  constructor(props) {
-    super();
-  }
+const App = () => {
+  const [orders, setOrders] = useState();
 
-  componentDidMount() {
-    getOrders().catch((err) => console.error('Error fetching:', err));
-  }
+  const getOrders = async () => {
+    const res = await fetchOrders();
+    const ordersData = await res.json();
+    setOrders(ordersData);
+  };
+  useEffect(() => {
+    try {
+      getOrders();
+    } catch (error) {
+      console.log(error.message);
+    }
+  });
 
-  render() {
-    return (
-      <main className="App">
-        <header>
-          <h1>Burrito Builder</h1>
-          <OrderForm />
-        </header>
-
-        {/* <Orders /> */}
-      </main>
-    );
-  }
-}
+  return (
+    <main className="App">
+      <header>
+        <h1>Burrito Builder</h1>
+        <OrderForm />
+      </header>
+      {/* <Orders /> */}
+    </main>
+  );
+};
 
 export default App;
