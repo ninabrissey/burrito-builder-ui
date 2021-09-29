82e408cdcb513325718e38ae6bc598ad37ab1799
Nina Brissey <nbrissey@gmail.com>
2021-09-29 09:55:44 -0700

    Add post fetch

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,3 +1,13 @@
 export const fetchOrders = () => {
   return fetch('http://localhost:3001/api/v1/orders');
 };
+
+export const postOrder = (newOrder) => {
+  return fetch('http://localhost:3001/api/v1/orders', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(newOrder),
+  });
+};
